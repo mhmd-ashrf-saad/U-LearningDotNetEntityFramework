e29cc3d374944b7945a179280e275528f4f22950
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-05-18 10:27:51 +0300

    updates

---
+++ U-LearningNET/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using U_LearningNET.Models;
+
+namespace U_LearningNET.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+}
